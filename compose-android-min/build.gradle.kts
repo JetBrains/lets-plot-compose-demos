7a947976927067b16db3f2774a6a0f28799f52de
Igor Alshannikov <Igor.Alshannikov@jetbrains.com>
2024-08-29 18:27:15 -0400

    Add tasks for downloading and "installing" Skiko binaries for Android.

--- compose-android-min/build.gradle.kts
+++ compose-android-min/build.gradle.kts
@@ -1,10 +1,10 @@
-import com.android.build.gradle.tasks.MergeSourceSetFolders
-import org.jetbrains.kotlin.gradle.tasks.KotlinJvmCompile
-
 /*
  * Copyright (c) 2023. JetBrains s.r.o.
  * Use of this source code is governed by the MIT license that can be found in the LICENSE file.
  */
+
+import com.android.build.gradle.tasks.MergeSourceSetFolders
+import java.net.URL
 
 plugins {
     kotlin("android")
@@ -15,23 +15,6 @@ plugins {
 
 val skikoNativeX64: Configuration by configurations.creating
 val skikoNativeArm64: Configuration by configurations.creating
-
-val copyJniLibs = tasks.register("copyJniLibs", Copy::class) {
-    val srcJniLibsDir = "${project.rootProject.projectDir}/skiko-jni-libs/"
-    val dstJniLibsDir = "${project.projectDir}/src/main/jniLibs/"
-
-    from(srcJniLibsDir)
-    into(dstJniLibsDir)
-    include("**/*")
-}
-
-tasks.withType<MergeSourceSetFolders>().configureEach {
-    dependsOn(copyJniLibs)
-}
-
-tasks.withType<KotlinJvmCompile>().configureEach {
-    dependsOn(copyJniLibs)
-}
 
 android {
     compileSdk = (findProperty("android.compileSdk") as String).toInt()
@@ -102,3 +85,67 @@ dependencies {
     implementation("org.slf4j:slf4j-api:2.0.9")
     implementation("com.github.tony19:logback-android:3.0.0")
 }
+
+
+////////////////////////////////////////////////////////
+// Include the following code in your Gradle build script
+// to ensure that compatible Skiko binaries are
+// downloaded and included in your project.
+//
+// Without this, you won't be able to run your app
+// in the IDE on a device emulator.
+// //////////////////////////////////////////////////////
+
+val skikoJniLibsReleaseAssetName = "skiko-jni-libs.zip"
+val skikoJniLibsDestDir = file("${project.projectDir}/src/main/jniLibs/")
+
+tasks.register("downloadSkikoJniLibsReleaseAsset") {
+    val repoUrl = "https://github.com/JetBrains/lets-plot-skia"
+    val releaseTag = "v$letsPlotSkiaVersion"
+
+    doLast {
+        val downloadUrl = "$repoUrl/releases/download/$releaseTag/$skikoJniLibsReleaseAssetName"
+        val outputFile = layout.buildDirectory.file("downloads/$skikoJniLibsReleaseAssetName").get().asFile
+
+        if (outputFile.exists()) {
+            println("File already exists: ${outputFile.absolutePath}")
+            println("Skipping download.")
+        } else {
+            outputFile.parentFile?.mkdirs()
+
+            println("Downloading $skikoJniLibsReleaseAssetName from $downloadUrl")
+            URL(downloadUrl).openStream().use { input ->
+                outputFile.outputStream().use { output ->
+                    input.copyTo(output)
+                }
+            }
+            println("Download completed: ${outputFile.absolutePath}")
+        }
+    }
+}
+
+tasks.register<Copy>("unzipSkikoJniLibsReleaseAsset") {
+    dependsOn("downloadSkikoJniLibsReleaseAsset")
+    from(zipTree(layout.buildDirectory.file("downloads/$skikoJniLibsReleaseAssetName")))
+    into(skikoJniLibsDestDir)
+    doFirst {
+        delete(skikoJniLibsDestDir)
+    }
+}
+
+tasks.register("cleanSkikoJniLibs") {
+    doLast {
+        delete(skikoJniLibsDestDir)
+    }
+}
+
+tasks.named("clean") {
+    dependsOn("cleanSkikoJniLibs")
+}
+
+tasks.withType<MergeSourceSetFolders>().configureEach {
+    dependsOn("unzipSkikoJniLibsReleaseAsset")
+}
+
+////////////////////////////////////////////////////////
+
