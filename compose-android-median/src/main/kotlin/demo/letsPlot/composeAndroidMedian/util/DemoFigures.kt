0b51b99824dc1f2ff2927f0bc8200f24aecf23b6
Ivan Kupriyanov <horis.ikupriyanov@gmail.com>
2026-05-28 15:09:44 +0200

    Switch to lets-plot-kotlin instead of lets-plot-kotlin-kernel, add datetime plot to android median demo

--- compose-android-median/src/main/kotlin/demo/letsPlot/composeAndroidMedian/util/DemoFigures.kt
+++ compose-android-median/src/main/kotlin/demo/letsPlot/composeAndroidMedian/util/DemoFigures.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2023 JetBrains s.r.o.
+ * Copyright (c) 2026. JetBrains s.r.o.
  * Use of this source code is governed by the MIT license that can be found in the LICENSE file.
  */
 
@@ -8,17 +8,22 @@ package demo.letsPlot.composeAndroidMedian.util
 import org.jetbrains.letsPlot.Figure
 import org.jetbrains.letsPlot.Stat
 import org.jetbrains.letsPlot.annotations.layerLabels
-import org.jetbrains.letsPlot.geom.geomBar
-import org.jetbrains.letsPlot.geom.geomDensity
-import org.jetbrains.letsPlot.geom.geomPie
+import org.jetbrains.letsPlot.geom.*
+import org.jetbrains.letsPlot.label.ggtitle
+import org.jetbrains.letsPlot.label.xlab
+import org.jetbrains.letsPlot.label.ylab
 import org.jetbrains.letsPlot.letsPlot
+import org.jetbrains.letsPlot.scale.scaleXDateTime
 import org.jetbrains.letsPlot.themes.themeVoid
+import org.jetbrains.letsPlot.tooltips.layerTooltips
+import kotlin.time.Instant
 
 fun createFigures(): List<Pair<String, Figure>> {
     return listOf(
         "Density" to densityPlot(),
         "Bar" to barPlot(),
         "Pie" to piePlot(),
+        "Datetime" to datetimePlot(),
     )
 }
 
@@ -56,4 +61,41 @@ fun piePlot(): Figure {
                 slice = "value"
                 fill = "name"
             } + themeVoid()
+}
+
+private fun datetimePlot(): Figure {
+    val data = mapOf(
+        "instant" to listOf(
+            Instant.parse("2026-05-01T00:00:00Z"),
+            Instant.parse("2026-05-01T06:00:00Z"),
+            Instant.parse("2026-05-01T12:00:00Z"),
+            Instant.parse("2026-05-01T18:00:00Z"),
+            Instant.parse("2026-05-02T00:00:00Z"),
+            Instant.parse("2026-05-02T06:00:00Z"),
+        ),
+        "throughput" to listOf(18.0, 24.0, 21.5, 28.0, 25.0, 31.0),
+    )
+
+    return letsPlot(data) {
+        x = "instant"
+        y = "throughput"
+    } +
+            geomLine(
+                color = "#4C78A8",
+                size = 1.6,
+                tooltips = layerTooltips()
+                    .line("@instant|@throughput req/s")
+                    .format("instant", "%Y-%m-%d %H:%M UTC"),
+            ) +
+            geomPoint(
+                color = "#E45756",
+                size = 4.0,
+                tooltips = layerTooltips()
+                    .line("@instant|@throughput req/s")
+                    .format("instant", "%Y-%m-%d %H:%M UTC"),
+            ) +
+            scaleXDateTime(format = "%b %d\n%H:%M") +
+            ggtitle("kotlinx.datetime.Instant values in plot") +
+            xlab("Instant (UTC)") +
+            ylab("Throughput")
 }
