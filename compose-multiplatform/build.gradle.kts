0488ec1202231452a2720ddb1f884f1294c93402
Igor Alshannikov <Igor.Alshannikov@jetbrains.com>
2026-06-30 16:54:32 -0400

    Upgrade deps.

--- compose-multiplatform/build.gradle.kts
+++ compose-multiplatform/build.gradle.kts
@@ -7,11 +7,6 @@
 
 import org.jetbrains.kotlin.gradle.ExperimentalWasmDsl
 
-/*
- * Copyright (c) 2025 JetBrains s.r.o.
- * Use of this source code is governed by the MIT license that can be found in the LICENSE file.
- */
-
 plugins {
     kotlin("multiplatform")
     kotlin("plugin.compose")
